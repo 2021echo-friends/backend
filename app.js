1fb00b9612f8cc1448ccc3cd9181bfd6fdd28c3a
ParkSeChan <hasmi5452@doubled.kr>
2021-08-06 15:20:45 +0900

    etc : 작은 수정

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/", routers);
 app.use(handlingError);
-app.all.apply("*", notFoundRouterError);
+app.all("*", notFoundRouterError);
 
 app.listen(3001, "0.0.0.0", () => {
   console.log("listening on port 3001");
