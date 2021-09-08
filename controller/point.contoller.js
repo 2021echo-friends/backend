08cffbb3b38f29abcb14886f093c521d92b0fd4a
psc <hasmi5452@gmail.com>
2021-09-08 20:38:12 +0900

    feat : 퀴즈 맞히기

--- controller/point.contoller.js
+++ controller/point.contoller.js
@@ -79,10 +79,11 @@ export const buyProduct = async (product_id, user_id) => {
   // point
   // eco_effect
   // coupon
+  let pointHistory;
   let coupon;
   await session.withTransaction(async () => {
     const product = await Product.findById(product_id);
-    await Point.findOneAndUpdate(
+    const point = await Point.findOneAndUpdate(
       { user_id },
       {
         $inc: {
@@ -103,8 +104,69 @@ export const buyProduct = async (product_id, user_id) => {
       },
       { session }
     );
+    pointHistory = (
+      await PointHistory.create(
+        [
+          {
+            point_id: point._id,
+            description:
+              product.name +
+              "를 구매하여, \n" +
+              product.point_value +
+              "의 포인트를 소비, \n" +
+              "이산화탄소는 " +
+              product.eco_value_co2 +
+              "L" +
+              "오존은 " +
+              product.eco_value_o3 +
+              "L" +
+              "메탄는 " +
+              product.eco_value_ch4 +
+              "L" +
+              "만큼 좋은 영향을 주었습니다.",
+            point_value: -product.point_value,
+            eco_value_co2: product.eco_value_co2,
+            eco_value_o3: product.eco_value_o3,
+            eco_value_ch4: product.eco_value_ch4,
+          },
+        ],
+        { session }
+      )
+    )[0];
     coupon = await Coupon.create([{ product_id }], { session });
   });
   await session.endSession();
   return coupon;
+};
+export const correctQuiz = async (user_id) => {
+  const session = await mongoose.startSession();
+  let pointHistory;
+  await session.withTransaction(async () => {
+    const point = await Point.findOneAndUpdate(
+      { user_id },
+      {
+        $inc: {
+          account: 1000,
+        },
+      },
+      { session }
+    );
+    pointHistory = (
+      await PointHistory.create(
+        [
+          {
+            point_id: point._id,
+            description: "출석퀴즈",
+            point_value: 1000,
+            eco_value_co2: 0,
+            eco_value_o3: 0,
+            eco_value_ch4: 0,
+          },
+        ],
+        { session }
+      )
+    )[0];
+  });
+  await session.endSession();
+  return pointHistory;
 };
--- routers/client/quiz.js
+++ routers/client/quiz.js
@@ -1,6 +1,7 @@
 // 출석체크 퀴즈 보기
 // 출석체크 보상 받기
 import { Router } from "express";
+import { correctQuiz } from "../../controller/point.contoller.js";
 import { getQuiz } from "../../controller/quiz.controller.js";
 import { responseHandler } from "../../lib/common.js";
 const router = Router();
@@ -9,6 +10,12 @@ router.get(
   "/",
   responseHandler(async (req) => {
     return getQuiz(req.query.len ? req.query.len : 3);
+  })
+);
+router.post(
+  "/",
+  responseHandler(async (req) => {
+    return correctQuiz(req.user._id);
   })
 );
 
